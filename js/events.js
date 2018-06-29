3e83e4495f5adf4a1e007d3ff24fead081cced43
Sarah Jones <saraht.jones8@gmail.com>
2018-06-29 19:35:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(){
+    window.alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+    if (key.which == 71){
+      window.alert('G was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    window.alert('Your form is going to be submitted now.');
+  })
+}
+
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
