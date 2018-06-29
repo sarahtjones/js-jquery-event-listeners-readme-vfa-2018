e876cafb66617410d1ac9a073a448db040c1561b
Sarah Jones <saraht.jones8@gmail.com>
2018-06-29 19:32:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,14 +22,9 @@ function pressIt() {
 
 //+ Define a function `submitIt` that does not accept a parameter.
 //The function should bind a `submit` event to the form that alerts `"Your form is going to be submitted now."`.
-function submitIt() {
-  $('form').on('submit', function(){
-    window.alert('Your form is going to be submitted now.');
-  })
-}
 
 $(document).ready(function(){
-  
+
   getIt();
   frameIt();
   pressIt();
